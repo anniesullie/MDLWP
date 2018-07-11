7e527c3735abe638ad1c0a269ad939780b3eb660
Annie Sullivan <sullivan@chromium.org>
2018-07-10 21:31:09 -0400

    Updated theme description and fixed category chip styling

--- template-parts/content.php
+++ template-parts/content.php
@@ -64,16 +64,18 @@
 	        <?php if ( 'post' == get_post_type() ) : ?>
 			<div class="entry-meta">
 				<?php mdlwp_posted_on(); ?>
+				<div class="category-chip-container">
 				<?php
 					$categories = get_the_terms( $id, 'category' );
 					if ( ! $categories || is_wp_error( $categories ) )
 						$categories = array();
 					foreach ($categories as $category) {
 						echo '<span class="mdl-chip">';
-						echo '	<span class="mdl-chip__text"><a href="' . esc_url(get_category_link($category->term_id)) . '">' . esc_html($category->name) . '</a>';
+						echo '	<span class="mdl-chip__text"><a href="' . esc_url(get_category_link($category->term_id)) . '">' . esc_html($category->name) . '</a></span>';
 						echo '</span>';
 					}
 				?>
+				</div>
 			</div><!-- .entry-meta -->
 			<?php endif; ?>
 	              
